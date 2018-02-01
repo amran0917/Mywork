e60940d02a4ae20b86167ac3e67dd5698f1e4a13
amran0917 <bsse0917@iit.du.ac.bd>
2018-02-01 09:38:34 +0600

    new commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# filef
+# filef
+# filef
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"hello world.com"<<endl;
+    int x=5;
+
+    int b = 3;
+
+            for(int i=10;i>=0;i--)
+     {
+                int c = x+b;
+                 cout<<c<<endl;
+    }
+   }
